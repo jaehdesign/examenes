333fe6360b189887e3eb6b3e9385daf0e4aec47e
juanan <jaehdesign@gmailcom>
2025-01-13 13:59:33 +0100

    Update content proyect v7

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 console.log("hola mundo");
-
-
-function EvaluateItems {
-
-const list = document.querySelectorAll("#listNumbers li");
-
-list.forEach(buttons => (item,n) {
-  console.log(n,item);/*Use variable item for any need*/
-});
-
-
-
-
-}
-
-
